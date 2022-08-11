c8b87a3b5145b1bf83a62fce9a2c3bbd6925446d
HJ <HJ@HJ>
2022-08-11 12:47:36 +0900

    0811

---
+++ boardTest2/src/main/java/wg/kim/boardtest/board/service/test.java
@@ -0,0 +1,45 @@
+package wg.kim.boardtest.board.service;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class test {
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("katamari.hj@gmail.com", "hetgmqigseawvtim");
+			}
+		});
+
+		String receiver = "katamari.hj@gmail.com"; // 메일 받을 주소
+		String title = "테스트 메일입니다아아앙아.";
+		String content = "<h2 style='color:blue'>안녕하세요</h2>";
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("katamari.hj@gmail.com", "관리자", "utf-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(content, "text/html; charset=utf-8");
+
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
